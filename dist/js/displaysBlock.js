97f87a56d3e348a98b58052bf7f011ce130df55e
Fat_Flow <valerkalol2014@gmail.com>
2024-01-12 17:48:43 +0200

    displays-block updated

--- dist/js/displaysBlock.js
+++ dist/js/displaysBlock.js
@@ -1,17 +1,15 @@
+const videoDisplay = document.querySelector('.displays-block_video-display')
 const otherDisplayContainer = document.querySelector(
     '.displays-block_other-displays'
 )
-const videoDisplay = document.querySelector('.displays-block_video-display')
-
 const otherDisplays = otherDisplayContainer.querySelectorAll(
     '.displays-block_display'
 )
 
-function controlVideo(vidFunc) {
-    if (!document.getElementById('play')) return
+const controlVideo = (vidFunc) => {
+    const iframe = document.querySelector('#play')
 
-    const iframe = document.getElementById('play').contentWindow
-    iframe.postMessage(
+    iframe?.contentWindow.postMessage(
         '{"event":"command","func":"' + vidFunc + '","args":""}',
         '*'
     )
@@ -21,15 +19,23 @@ const displayBlockScrollHandler = () => {
     const { y, height, top } = otherDisplayContainer.getBoundingClientRect()
 
     if (y < 300) {
+        if (
+            otherDisplayContainer.classList.contains(
+                'displays-block_other-displays_active'
+            )
+        ) {
+            controlVideo('playVideo')
+        }
+
         otherDisplayContainer.classList.add(
             'displays-block_other-displays_active'
         )
-        controlVideo('playVideo')
     }
     if (y + height < 200 || top - window.innerHeight > 0) {
         otherDisplayContainer.classList.remove(
             'displays-block_other-displays_active'
         )
+
         controlVideo('pauseVideo')
     }
 }
@@ -37,6 +43,7 @@ const displayBlockScrollHandler = () => {
 const openVideo = (
     videoSrc = 'https://www.youtube.com/embed/bd-KpqIyLUk?autoplay=1&enablejsapi=1&html5=1'
 ) => {
+    if (document.querySelector('#play')) return
     const iframe = document.createElement('iframe')
     iframe.src = videoSrc
     iframe.setAttribute('id', 'play')
